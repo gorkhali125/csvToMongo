a4125bb1b146009f964ba6a0b28209deab897957
gorkhali125 <deepeshkhanal@gmail.com>
2023-06-14 22:30:08 +0545

    Define other required config

--- csvToMongo.py
+++ csvToMongo.py
@@ -7,9 +7,14 @@ host_url = 'yourMongoHostUrl'
 mongo_host = 'mongodb+srv://' + mongo_user + ':' + mongo_password + '@' + host_url
 mongo_port = 27017
 
-#define schema and collection name
+# define schema and collection name
 db_name = 'yourDBName'
 collection_name = 'yourCollectionName'
+
+# define other required variables
+batch_size = 100
+timeout_seconds = 300
+input_file_name = 'yourCSVFile.csv' # Note that this should be in the same directory as this script
 
 # define mongo client, db and collection
 client = MongoClient(mongo_host, mongo_port)
