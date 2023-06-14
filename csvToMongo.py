8d7873e76a77c35c0b2569549e0db2379ce9db06
gorkhali125 <deepeshkhanal@gmail.com>
2023-06-14 22:27:29 +0545

    Add db and collection config

--- csvToMongo.py
+++ csvToMongo.py
@@ -7,4 +7,11 @@ host_url = 'yourMongoHostUrl'
 mongo_host = 'mongodb+srv://' + mongo_user + ':' + mongo_password + '@' + host_url
 mongo_port = 27017
 
+#define schema and collection name
+db_name = 'yourDBName'
+collection_name = 'yourCollectionName'
+
+# define mongo client, db and collection
 client = MongoClient(mongo_host, mongo_port)
+db = client[db_name]
+collection = db[collection_name]
