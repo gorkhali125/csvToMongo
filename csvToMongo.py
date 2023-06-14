219d9a0832269ea87e9b08b0d2c0766409e5f277
gorkhali125 <deepeshkhanal@gmail.com>
2023-06-14 22:35:44 +0545

    Implement the import process

--- csvToMongo.py
+++ csvToMongo.py
@@ -1,11 +1,13 @@
+import csv
 from pymongo import MongoClient
+from datetime import datetime
 
-# define some required configurations
+# define mongo configurations
 mongo_user = 'yourMongoUserName'
 mongo_password = 'yourMongoPassword'
 host_url = 'yourMongoHostUrl'
-mongo_host = 'mongodb+srv://' + mongo_user + ':' + mongo_password + '@' + host_url
 mongo_port = 27017
+mongo_host = 'mongodb+srv://' + mongo_user + ':' + mongo_password + '@' + host_url
 
 # define schema and collection name
 db_name = 'yourDBName'
@@ -20,3 +22,27 @@ input_file_name = 'yourCSVFile.csv' # Note that this should be in the same direc
 client = MongoClient(mongo_host, mongo_port)
 db = client[db_name]
 collection = db[collection_name]
+
+# begin the actual import process
+with open(input_file_name) as file:
+    reader = csv.DictReader(file)
+    batch = []
+    last_insert_time = datetime.now()
+    for i, row in enumerate(reader):
+        try:
+            batch.append(row)
+            if len(batch) == batch_size or (datetime.now() - last_insert_time).total_seconds() >= timeout_seconds:
+                result = collection.insert_many(batch)
+                if result.acknowledged:
+                    batch = []
+                    last_insert_time = datetime.now()
+                    print(f'Processed {i+1} records')
+        except Exception as e:
+            print(f'Error on row {i+1}: {e}')
+    if batch:
+        try:
+            result = collection.insert_many(batch)
+            if result.acknowledged:
+                print(f'Processed {i+1} records')
+        except Exception as e:
+            print(f'Error on row {i+1}: {e}')
